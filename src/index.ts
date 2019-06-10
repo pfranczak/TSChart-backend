0531a14c875ff17415ff74be6a9706527c03f61f
pfranczak <franczak.przemyslaw3@gmail.com>
2019-06-10 22:15:57 +0200

    change lint rule

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-import {SERVER_PORT} from './config';
-
+const PORT = 3001;
 const app = express();
 
 // define a route handler for the default home page
-app.get( '/', ( req, res ) => {
-    res.send( 'Hello world!' );
-} );
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
 
 // start the Express server
-app.listen(SERVER_PORT, () => {
-    console.log( `server started at http://localhost:${ SERVER_PORT }` );
-} );
+app.listen(PORT, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${PORT}`);
+});
